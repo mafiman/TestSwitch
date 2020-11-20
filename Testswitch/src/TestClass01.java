0f5ee9f3ad9da467661b671c6a59656c6b97e860
martin <martin@Gladice>
2020-11-20 19:54:25 +0100

    Version 02 moved

---
+++ Testswitch/src/TestClass01.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+
+/**
+ * @author martin
+ *
+ */
+public class TestClass01 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
